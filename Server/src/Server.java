9c8ae736fb9b3a30b86174b52806eef089c52b59
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2019-09-26 11:11:05 +0200

    Merge branch 'server' of C:\Users\maije\Documents\GitHub\CodenameUltra with conflicts.

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,5 +1,11 @@
+
 import javafx.application.Application;
 
 abstract class Server extends Application {
+<<<<<<< HEAD
 
 }
+=======
+private int sessionNo=1;
+}
+>>>>>>> 1be753d4c214c737ba7daa3de50ba73f01d21235
