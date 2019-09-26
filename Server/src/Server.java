c0d15c0e0ce3c5a61cf3c1a2ce4bbc22f225d2a7
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2019-09-26 11:33:36 +0200

    test

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -2,6 +2,7 @@
 import javafx.application.Application;
 
 abstract class Server extends Application {
-private int sessionNo=1;
-private int person = 0;
+
+    private int sessionNo = 1;
+    private int person = 0;
 }
