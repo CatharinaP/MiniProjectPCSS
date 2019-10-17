50783589754175c58255de58bd9b805031e91640
Elizabete <eavoti18@student.aau.dk>
2019-10-17 14:49:34 +0200

    new Thread

--- Server/src/HandleASession.java
+++ Server/src/HandleASession.java
@@ -3,19 +3,33 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class HandleASession implements Runnable, Constants {
-private Socket socket;
+class HandleASession implements Runnable, Constants {
+private Socket socket; // a connected socket
 
 public HandleASession(Socket socket){
     this.socket = socket;
 }
-    @Override
+
     public void run() {
 try {
-    DataInputStream inputFromClient = new DataInputStream(
-            socket.getInputStream());
-    DataOutputStream OutputFromClient = new DataOutputStream(
-            socket.getOutputStream());
+    // create data input and output streams
+    DataInputStream inputFromClient1 = new DataInputStream(socket.getInputStream());
+    DataInputStream inputFromClient2 = new DataInputStream(socket.getInnputStream());
+
+    DataOutputStream OutputToClient1 = new DataOutputStream(socket.getOutputStream());
+    DataOutputStream OutputToClient2 = new DataOUtputStream(socket.getOUtputStream());
+
+    // for serving the client
+    while (true){
+        // data exchange between the client and server
+
+        // based on information from client 1, send the message to client 2
+
+        string messageFromClient1 = inputFromClient1.readString();
+
+        outputToClient2.writeString();
+
+    }
 
 } catch (IOException e) {
     e.printStackTrace();
