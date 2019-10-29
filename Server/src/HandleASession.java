616d29a4693ce6d2d8a2ea1198da78d5dc984b61
Elizabete <eavoti18@student.aau.dk>
2019-10-29 11:16:06 +0100

    sorry only 2 players then.....

--- Server/src/HandleASession.java
+++ Server/src/HandleASession.java
@@ -12,29 +12,23 @@ private Socket player4;
 public HandleASession(Socket player1, Socket player2){
     this.player1 = player1;
     this.player2 = player2;
-    this.player3 = player3;
-    this.player4 = player4;
 }
 
 
     public void run() {
 try {
-    // input streams for 4 clients
+    // input streams for 2 clients
     // another commit
 
     DataInputStream inputFromClient1 = new DataInputStream(player1.getInputStream());
     DataInputStream inputFromClient2 = new DataInputStream(player2.getInputStream());
 
-    DataInputStream inputFromClient3 = new DataInputStream(player3.getInputStream());
-    DataInputStream inputFromClient4 = new DataInputStream(player4.getInputStream());
 
-    // output streams for 4 clients
+    // output streams for 2 clients
 
     DataOutputStream outputToClient1 = new DataOutputStream(player1.getOutputStream());
     DataOutputStream outputToClient2 = new DataOutputStream(player2.getOutputStream());
 
-    DataOutputStream outputToClient3 = new DataOutputStream(player3.getOutputStream());
-    DataOutputStream outputToClient4 = new DataOutputStream(player4.getOutputStream());
 
     // for serving the client
     while (true){
@@ -50,12 +44,6 @@ try {
 
         double fromPlayerSecond = inputFromClient2.readDouble();
         System.out.println("Reading double playerSecond");
-
-        double fromPlayerThird = inputFromClient3.readDouble();
-        System.out.println("Reading double playerThird");
-
-        double fromPlayerForth = inputFromClient4.readDouble();
-        System.out.println("Reading double playerForth");
 
         // if 2 people in 1 room, start the game
 
@@ -74,22 +62,7 @@ try {
             System.out.println("Player2 an Intelligens");
 
         }
-
-        if (fromPlayerThird == 3 && fromPlayerForth == 4){
-
-            // start another game in another specific room
-            System.out.println("Third and Forth player have requested to play");
-
-            // assigning character
-            // sending to server "10" meaning player1 will be a War Officer
-            outputToClient3.writeDouble(10);
-            System.out.println("Player3 a War Officer");
-
-            // assigning character
-            // sending to server "20" meaning player2 will be an Intelligens
-            outputToClient4.writeDouble(20);
-            System.out.println("Player4 an Intelligens");
-        }
+        
 
         // received turing = true
 
