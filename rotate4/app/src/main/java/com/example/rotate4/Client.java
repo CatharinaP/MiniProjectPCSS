2155a8d0bda394c479110bb6d295f171b34e4ef2
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2019-10-29 11:56:58 +0100

    Added changes page when game start

--- rotate4/app/src/main/java/com/example/rotate4/Client.java
+++ rotate4/app/src/main/java/com/example/rotate4/Client.java
@@ -1,15 +1,21 @@
 package com.example.rotate4;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
-    public static void main(String[] args){
+public class Client extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         System.out.println("Class Client is working pls");
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
         boolean connect = true;
 
         try {
@@ -20,7 +26,7 @@ public class Client {
             DataInputStream inputStream = new DataInputStream(s.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
 
-            while (connect){
+            while (connect) {
                 System.out.print("While connected...");
                 outputStream.writeDouble(1);
                 System.out.print("Sending double '1' to server for defining ");
@@ -28,19 +34,31 @@ public class Client {
                 double warOfficer = inputStream.readDouble();
                 double intelligens = inputStream.readDouble();
 
-                if (warOfficer == 10){
+                if (warOfficer == 10) {
                     // change the screen with intent
+                    startwar();
                 }
 
                 if (intelligens == 20) {
                     // change the screen with intent
+                    startintel();
                 }
-                
+
 
             }
-        } catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println("ex");
         }
 
+    }
+
+    void startwar(){
+        Intent intent = new Intent(this, DesktopWar.class);
+        startActivity(intent);
+    }
+
+    void startintel(){
+        Intent intent = new Intent(this,Desktop.class);
+        startActivity(intent);
     }
 }
